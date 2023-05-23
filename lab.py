b404487fdadda2e7f8ec65e425c068c54d3abd10
Dominik <dominik7550j@gmail.com>
2023-05-23 14:33:40 +0200

    task2

--- lab.py
+++ lab.py
@@ -1,3 +1,34 @@
+
+#  TASK 0
+
+#!/usr/bin/env python
+
+# Lista komponentow
+required_packages = [
+    "sys",
+    "json",
+    "os",
+    "subprocess",
+    
+]
+
+if __name__ == '__main__':
+    import subprocess
+
+    for package in required_packages:
+        try:
+            subprocess.check_call(['pip', 'install', package])
+        except subprocess.CalledProcessError:
+            print(f'Błąd podczas instalacji pakietu {package}.')
+    print('Wszystkie wymagane pakiety zostały zainstalowane.')
+
+
+#  TASK 1
+import sys
+
+arguments = sys.argv[1:]
+print("Passed arguments:", arguments)
+
 #Task 2
 import json
 import os
